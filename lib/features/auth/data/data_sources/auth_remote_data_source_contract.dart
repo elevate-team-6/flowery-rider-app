49ee5f86a3bfb454ad68e257b85fe64fa4d31beb
HD <ahmedmohamedemam55@gmail.com>
2026-06-16 06:37:51 +0300

    feat: implement authentication models and responses for user sign-in

--- lib/features/auth/data/data_sources/auth_remote_data_source_contract.dart
+++ lib/features/auth/data/data_sources/auth_remote_data_source_contract.dart
@@ -1,1 +1,7 @@
-abstract interface class AuthRemoteDataSourceContract {}
+import 'package:flowery_rider_app/config/base_response/base_response.dart';
+import 'package:flowery_rider_app/features/auth/data/models/request/sign_in_request_model.dart';
+import 'package:flowery_rider_app/features/auth/data/models/response/sign_in_response_model.dart';
+
+abstract interface class AuthRemoteDataSourceContract {
+  Future<BaseResponse<SignInResponseModel>> signIn(SignInRequestModel request);
+}
