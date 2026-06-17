d462c4282455367558ca1451954f52b86ae6b82b
HD <ahmedmohamedemam55@gmail.com>
2026-06-17 18:51:50 +0300

    refactor: remove unused import for app_text_styles in custom_search_field.dart

--- lib/core/widgets/custom_search_field.dart
+++ lib/core/widgets/custom_search_field.dart
@@ -2,7 +2,6 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flowery_rider_app/core/utils/app_assets.dart';
 import 'package:flowery_rider_app/core/utils/app_colors.dart';
 import 'package:flowery_rider_app/core/utils/app_strings.dart';
-import 'package:flowery_rider_app/core/utils/app_text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
