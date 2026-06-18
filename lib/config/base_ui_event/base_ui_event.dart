9e9fadb5c9ef707603afe37e426771125123302f
HD <ahmedmohamedemam55@gmail.com>
2026-06-18 22:06:37 +0300

    refactor: remove unused ShowLoadingEvent and HideLoadingEvent classes

--- lib/config/base_ui_event/base_ui_event.dart
+++ lib/config/base_ui_event/base_ui_event.dart
@@ -6,10 +6,6 @@ class HideLoadingEvent extends BaseUiEvent {}
 
 enum NavigationType { push, pushReplacement, pushAndRemoveUntil, pop }
 
-class ShowLoadingEvent extends BaseUiEvent {}
-
-class HideLoadingEvent extends BaseUiEvent {}
-
 class DisplayErrorEvent extends BaseUiEvent {
   final String errorMessage;
   DisplayErrorEvent(this.errorMessage);
