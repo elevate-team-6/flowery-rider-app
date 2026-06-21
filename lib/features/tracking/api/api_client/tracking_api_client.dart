252eda1742c19336bdf76474a90ba794890e2946
HD <ahmedmohamedemam55@gmail.com>
2026-06-21 23:44:11 +0300

    feat(tracking): add pending orders API endpoint and data source implementation

--- lib/features/tracking/api/api_client/tracking_api_client.dart
+++ lib/features/tracking/api/api_client/tracking_api_client.dart
@@ -1,7 +1,10 @@
 import 'package:dio/dio.dart';
 import 'package:flowery_rider_app/core/utils/app_end_points.dart';
+import 'package:flowery_rider_app/core/utils/app_params.dart';
 import 'package:injectable/injectable.dart';
 import 'package:retrofit/retrofit.dart';
+
+import '../../data/models/response/pending_orders_response_model.dart';
 
 part 'tracking_api_client.g.dart';
 
@@ -10,6 +13,11 @@ part 'tracking_api_client.g.dart';
 abstract class TrackingApiClient {
   @factoryMethod
   factory TrackingApiClient(Dio dio) = _TrackingApiClient;
+
+  @GET(AppEndPoints.pendingOrders)
+  Future<PendingOrdersResponseModel> getPendingOrders({
+    @Query(ApiParameters.pageQueryParam) int? page,
+  });
 
   @GET(AppEndPoints.logout)
   Future<void> logout();
