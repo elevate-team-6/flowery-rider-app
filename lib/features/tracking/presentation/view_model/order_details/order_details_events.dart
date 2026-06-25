30eb8b56126436b5ac28f467d148392caa4167bf
Abdelmalek <m7244985@gmail.com>
2026-06-26 02:47:52 +0300

    fix(tracking): correct store phone field access and update UI event handling

--- lib/features/tracking/presentation/view_model/order_details/order_details_events.dart
+++ lib/features/tracking/presentation/view_model/order_details/order_details_events.dart
@@ -1,12 +1,19 @@
+import 'package:equatable/equatable.dart';
 import 'package:flowery_rider_app/features/tracking/domain/entities/order_entity.dart';
 
-sealed class OrderDetailsEvents {}
+sealed class OrderDetailsEvents extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
 
 class InitializeOrderDetailsEvent extends OrderDetailsEvents {
   final OrderEntity order;
   final int? initialStep;
 
   InitializeOrderDetailsEvent(this.order, {this.initialStep});
+
+  @override
+  List<Object?> get props => [order, initialStep];
 }
 
 class NextStepEvent extends OrderDetailsEvents {}
@@ -17,24 +24,36 @@ class RevertOrderToPendingEvent extends OrderDetailsEvents {
   final String orderId;
 
   RevertOrderToPendingEvent(this.orderId);
+
+  @override
+  List<Object?> get props => [orderId];
 }
 
 class NavigateToMapEvent extends OrderDetailsEvents {
   final LocationType locationType;
 
   NavigateToMapEvent(this.locationType);
+
+  @override
+  List<Object?> get props => [locationType];
 }
 
 class CallPhoneEvent extends OrderDetailsEvents {
   final String phoneNumber;
 
   CallPhoneEvent(this.phoneNumber);
+
+  @override
+  List<Object?> get props => [phoneNumber];
 }
 
 class OpenWhatsAppEvent extends OrderDetailsEvents {
   final String phoneNumber;
 
   OpenWhatsAppEvent(this.phoneNumber);
+
+  @override
+  List<Object?> get props => [phoneNumber];
 }
 
 enum LocationType { store, user }
