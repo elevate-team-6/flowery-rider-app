d448497bcc3cecc2db4460a5a325c5d867295718
Abdelmalek <m7244985@gmail.com>
2026-06-27 00:06:35 +0300

    refactor(config): enhance DI and network security
    - Added 'externalDio' to DioModule for safe third-party API calls.
    - Refactored AuthInterceptor to restrict token injection to internal baseUrl only, preventing sensitive token leakage to external services (FCM, Google Fonts).
    - Added FirebaseCrashlytics to FirebaseModule for better dependency injection and testability.

--- lib/config/services/firebase_module.dart
+++ lib/config/services/firebase_module.dart
@@ -1,8 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:injectable/injectable.dart';
 
 @module
 abstract class FirebaseModule {
   @lazySingleton
   FirebaseFirestore get firestore => FirebaseFirestore.instance;
+
+  @lazySingleton
+  FirebaseCrashlytics get crashlytics => FirebaseCrashlytics.instance;
 }
